ad15e817ade5cb0a61ededdf88b09f87922f7f8f
Bartosz Domarski <bartek.domarski@gmail.com>
2023-02-25 23:51:24 +0100

    version 0.0.3

--- AutomaticBetting/popup/popup.js
+++ AutomaticBetting/popup/popup.js
@@ -6,14 +6,32 @@ chrome.storage.local.get(['availableSystems', 'elementsText', 'elementsUrl', 'el
   const elementsDataValue = result.elementsDataValue
   const list = document.getElementById('list')
 
-  console.log('availableSystems: ' + result.availableSystems.length)
-  console.log('elementsText: ' + result.elementsText.length)
-  console.log('elementsUrl: ' + result.elementsUrl.length)
-  console.log('elementsDataID: ' + result.elementsDataID.length)
-  console.log('elementsDataValue: ' + result.elementsDataValue.length)
+  function checkSystemOdds(system) {
+    const system477 = [[1,0,0,1,0,1,1],[0,1,1,0,0,1,1],[0,1,0,1,1,0,1],[1,0,1,0,1,0,1],[0,0,1,1,1,1,0],[1,1,0,0,1,1,0],[1,1,1,1,0,0,0]]
+    const system477Indexes = [[0,3,5,6],[1,2,5,6],[1,3,4,6],[0,2,4,6],[2,3,4,5],[0,1,4,5],[0,1,2,3]]
+    let badCoupons = []
 
-  function systems(system) {
-    console.log(system.currentTarget.textContent)
+    if (system.currentTarget.textContent == '4/7/7') {
+        for (let i = 0; i < system477Indexes.length; i++) {
+          let result = 1
+          system477Indexes[i].forEach((index) => {
+            result *= elementsDataValue[index]
+          })
+          if (result < 100) {
+            let ix = i + 1
+            let missingOdds = (100/result).toFixed(2)
+            badCoupons.push({key: ix, value: missingOdds})
+          }
+        }
+
+        if (badCoupons.length > 0) {
+          alert('W kuponach nr: ' + Object.keys(badCoupons).join() + ' brakuje takich kursow: ' + Object.values(badCoupons).join())
+        } else {
+          alert('Kursy sa ok!')
+        }
+    } else {
+      alert('System niedostępny')
+    }
   }
 
   const listSystems = document.createElement('div')
@@ -21,7 +39,7 @@ chrome.storage.local.get(['availableSystems', 'elementsText', 'elementsUrl', 'el
     const item = document.createElement('button')
     item.textContent = element
     item.setAttribute('type', 'button')
-    item.addEventListener('click', systems)
+    item.addEventListener('click', checkSystemOdds)
     listSystems.appendChild(item)
   });
   list.appendChild(listSystems)
